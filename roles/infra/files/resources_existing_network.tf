fd67faa44eefb87350c5cbbec5145a1274f94d53
Matt Anson <matta@stackhpc.com>
2022-01-12 20:20:22 +0000

    Use network UUID because port isn't in scope

--- roles/infra/files/resources_existing_network.tf
+++ roles/infra/files/resources_existing_network.tf
@@ -18,7 +18,7 @@ resource "openstack_compute_instance_v2" "capi_manager" {
   key_pair  = openstack_compute_keypair_v2.capi_manager_deploy.name
 
   network {
-    port = data.openstack_networking_port_v2.capi_manager.id
+    uuid = data.openstack_networking_network_v2.capi_manager.id
   }
 }
 
